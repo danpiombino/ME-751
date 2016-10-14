02a6da8f06cc5bf9b00bf741f4448f33b210a702
danpiombino <piombino@wisc.edu>
2016-10-13 19:23:01 -0500

    Add files via upload

--- phi_cd.m
+++ phi_cd.m
@@ -1,4 +1,4 @@
-function [phi,nu,gamma,phi_r,phi_p] = phi_dp1(q,q_d,i,j,ai_b,aj_b,theta)
+function [phi,nu,gamma,phi_r,phi_p] = phi_cd(q,q_d,i,j,sp_ib,sq_jb,c,dis)
 % Calculates parameters associated with DP1 constraint between coordinate
 % systems i and j
 % 
@@ -7,12 +7,14 @@ function [phi,nu,gamma,phi_r,phi_p] = phi_dp1(q,q_d,i,j,ai_b,aj_b,theta)
 %           i = Index of 1st coordinate system of interest (0 = ground)
 %           j = Index of 2nd coordinate system of interest (0 = ground)
 %           q_d = [7*nb x 1] vector of velocities in r and p directions
-%           ai_b = DP1 reference vector from coordinate system i (local)
-%           aj_b = DP1 reference vector from coordinate system j (local)
-%           theta = Desired angle between reference vectors, theta, and 
-%                   derivatives over time. Input should take the form:
-%                   [theta(t) theta_dot(t) theta_doubledot(t)]'
-%                   UNITS: [RAD RAD/S RAD/S/S]'
+%           sp_ib = vector in local coordinate system i to reference point
+%                   p: [3x1 column vector]
+%           sq_ib = vector in local coordinate system j to reference point
+%                   q: [3x1 column vector]
+%           c = vector in global coordinate system along which to enforce
+%               the constraint: [x y z]'
+%           dis = column vector of prescribed displacement function and
+%                 derivatives in the form of: [dis,dis_d,dis_dd]'
 %
 % Outputs:  phi = value of constraint evaluated at current conditions
 %           nu = coefficeints on right side of velocity equation
@@ -29,47 +31,51 @@ nb = length(q)/7;
 
 % Coordinate System i
 Ai = orient(q,i);
-ai = Ai*ai_b;
-Bi = bmat(q,i,ai_b);
-Bi_d = bmat(q_d,i,ai_b);
+Bi = bmat(q,i,sp_ib);
+Bi_d = bmat(q_d,i,sp_ib);
 
 if i == 0
+    ri = [0 0 0]'; %by definition
+    ri_d = [0 0 0]'; %by definition
     pi = [1 0 0 0]'; %by definition
     pi_d = zeros(4,1); %by definition
 else
+    ri = q([3*(i-1)+1:3*(i-1)+3]);
+    ri_d = q_d([3*(i-1)+1:3*(i-1)+3]);
     pi = q([4*(i-1)+3*nb+1:4*(i-1)+3*nb+4]);
     pi_d = q_d([4*(i-1)+3*nb+1:4*(i-1)+3*nb+4]);
 end
 
 % Coordinate System j
 Aj = orient(q,j);
-aj = Aj*aj_b;
-Bj = bmat(q,j,aj_b);
-Bj_d = bmat(q_d,j,aj_b);
+Bj = bmat(q,j,sq_jb);
+Bj_d = bmat(q_d,j,sq_jb);
 
 if j == 0
+    rj = [0 0 0]'; %by definition
+    rj_d = [0 0 0]'; %by definition
     pj = [1 0 0 0]'; %by definition
     pj_d = zeros(4,1); %by definition
 else
+    rj = q([3*(i-1)+1:3*(i-1)+3]);
+    rj_d = q_d([3*(i-1)+1:3*(i-1)+3]);
     pj = q([4*(j-1)+3*nb+1:4*(j-1)+3*nb+4]);
     pj_d = q_d([4*(j-1)+3*nb+1:4*(j-1)+3*nb+4]);
 end
 
-% Theta 
-f = norm(ai_b)*norm(aj_b)*cos(theta(1));
-f_d = -norm(ai_b)*norm(aj_b)*sin(theta(1))*theta(2);     
-f_dd = -norm(ai_b)*norm(aj_b)*(cos(theta(1))*theta(2)^2+sin(theta(1))*theta(3));
-
 %% Evaluate Quantities
+phi = c'*(rj+Aj*sq_jb-ri-Ai*sp_ib)-dis(1);
+nu = -c'*(rj_d+Bj*pj_d-ri_d-Bi*pi_d)+dis(2);
+gamma = c'*(Bi_d*pi_d-Bj_d*pj_d)+dis(3);\
 
-phi = ai_b'*Ai'*Aj*aj_b-f;
-nu = -ai'*Bj*pj_d-aj'*Bi*pi_d+f_d; %nu = -phi_t
-gamma = -ai'*Bj_d*pj_d-aj'*Bi_d*pi_d-2*(Bi*pi_d)'*(Bj*pj_d)+f_dd;
-
-phi_r = zeros(1,3*nb);  %by definition for DP1
+phi_r = zeros(1,3*nb);
+phi_ri = -c';
+phi_rj = c';
+phi_r(3*(i-1)+1:3*(i-1)+3) = phi_ri;
+phi_r(3*(j-1)+1:3*(j-1)+3) = phi_rj;
 phi_p = zeros(1,4*nb);
-phi_pi = aj_b'*Aj'*Bi;
-phi_pj = ai_b'*Ai'*Bj;
+phi_pi = -c'*Bi;
+phi_pj = c'*Bi;
 phi_p(4*(i-1)+1:4*(i-1)+4) = phi_pi;
 phi_p(4*(j-1)+1:4*(j-1)+4) = phi_pj;
 phi_q = [phi_r phi_p];
